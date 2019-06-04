48810cb6013a6e6a6fa393e2c846dbac9d7fc695
kasiakne <kasiakne@gmail.com>
2019-06-04 20:08:24 +0200

    First commit

---
+++ Assets/Scenes/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthUnits = 16f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+
+    }
+}
